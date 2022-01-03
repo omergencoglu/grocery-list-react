0087a9ae21d011c0eafdd13a52ade248c65246c9
Omer Gencoglu <omersaitgencoglu@gmail.com>
2022-01-02 22:06:10 -0500

    Created Item list component

--- src/components/ItemsList/ItemsList.js
+++ src/components/ItemsList/ItemsList.js
@@ -1,7 +1,16 @@
 import React from "react";
 
+import classes from "./ItemsList.module.css";
+
 const ItemsList = () => {
-  return <div></div>;
+  return (
+    <div className={classes.container}>
+      <li className={classes.listItem}>omer</li>
+      <li className={classes.listItem}>deneme</li>
+      <li className={classes.listItem}>deneme</li>
+      <button className={classes.clearButton}>Clear Items</button>
+    </div>
+  );
 };
 
 export default ItemsList;
